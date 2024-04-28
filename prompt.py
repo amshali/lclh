d7b6572789c284e677bcca8466e7df1e5a7e4c27
Am Shal <amshali@gmail.com>
2024-04-28 16:52:05 -0700

    Exanding the prompt to cover compilers and other tools

--- prompt.py
+++ prompt.py
@@ -2,7 +2,8 @@ from langchain.prompts import PromptTemplate
 
 command_help_prompt = PromptTemplate(
     template="""
-Return a bullet list of one-liner linux/unix command(s) for the following task/need.
+Return a bullet list of one-liner linux/unix command(s)
+or coding/programming related commands for the following task/need.
 (Only return the commands, no explanation needed.)
 
 If the input is not something related to linux commands, say "Cannot help with that."
